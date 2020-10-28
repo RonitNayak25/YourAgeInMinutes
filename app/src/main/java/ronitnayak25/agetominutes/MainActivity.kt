7d979200435f3b37b390906293d26ecdd47bb360
RonitNayak25 <ronitnayak25@gmail.com>
2020-10-29 02:55:34 +0530

    Calendar Input Added

--- app/src/main/java/ronitnayak25/agetominutes/MainActivity.kt
+++ app/src/main/java/ronitnayak25/agetominutes/MainActivity.kt
@@ -1,11 +1,17 @@
 package ronitnayak25.agetominutes
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,7 +22,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     fun clickDatePicker(view: View) {
-
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(
+                this,
+                DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                    Toast.makeText(this, "The chosen Date is $selectedDayOfMonth/$selectedMonth/$selectedYear", Toast.LENGTH_LONG).show()
+                },
+                year,
+                month,
+                day
+        )
+        dpd.show()
     }
 }
