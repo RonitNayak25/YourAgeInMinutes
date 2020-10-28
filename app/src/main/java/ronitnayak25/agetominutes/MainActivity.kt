eb65dd0753678ebbeed828aca949d94909021f3d
RonitNayak25 <ronitnayak25@gmail.com>
2020-10-29 02:48:54 +0530

    setOnClickListner on  button

--- app/src/main/java/ronitnayak25/agetominutes/MainActivity.kt
+++ app/src/main/java/ronitnayak25/agetominutes/MainActivity.kt
@@ -2,10 +2,21 @@ package ronitnayak25.agetominutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btnDatePicker)
+        button.setOnClickListener { view ->
+            clickDatePicker(view)
+        }
+    }
+
+    fun clickDatePicker(view: View) {
+
     }
 }
